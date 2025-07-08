d90485e9aeeef88e496bafb50eb86a3eeae9080b
miatrang15 <miatluong@gmail.com>
2025-07-07 23:08:32 -0400

    Complete Sprint 7 project setup with test framework

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
-# File with constants
+URBAN_ROUTES_URL = ' https://cnt-0cfc08a2-9cd1-420d-b6a4-20f8f7232dcc.containerhub.tripleten-services.com'
+
+# You'll also add these other constants:
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
